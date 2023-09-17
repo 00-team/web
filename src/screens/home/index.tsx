9cfe9c12e37ed4b9958922704c55de3eef69096a
sadra <sadrataghavi1383@gmail.com>
2023-09-17 15:11:58 +0330

    feat(home): init

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const Home: Component<{}> = () => {
+    return (
+        <main class="home">
+            <section class="hero"></section>
+        </main>
+    );
+};
+
+export { Home };
