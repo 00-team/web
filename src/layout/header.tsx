0ece9f8e9d5a9590249f7ab321dc4dddb7f61982
007 <dr007cc@gmail.com>
2023-09-17 13:06:03 +0330

    init

---
+++ src/layout/header.tsx
@@ -0,0 +1,13 @@
+import './style/header.scss'
+
+const Header = () => {
+    return (
+        <div class='header'>
+            <div class='logo'>a logo</div>
+            <div class='status'>hi from 00 Teamss</div>
+            <div></div>
+        </div>
+    )
+}
+
+export { Header }
