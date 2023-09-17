283f9f07026583eaf6c6fdccb286f2aa0bf7fde4
sadra <sadrataghavi1383@gmail.com>
2023-09-17 15:11:43 +0330

    feat(loading)

---
+++ src/layout/loading.tsx
@@ -0,0 +1,8 @@
+import { Component } from "solid-js";
+
+import "./style/loading.scss";
+
+const Loading: Component = () => {
+    return <div class="loading">loading...</div>;
+};
+export { Loading };
