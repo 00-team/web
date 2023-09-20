f388e3ffb720ddba93af071afc55472a2fa7bfc3
sadra <sadrataghavi1383@gmail.com>
2023-09-20 13:42:42 +0330

    feat(home)

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,12 @@
 import { Component } from "solid-js";
+import { HeroSection } from "./heroSection";
+
+import "./style/home.scss";
 
 const Home: Component<{}> = () => {
     return (
         <main class="home">
-            <section class="hero"></section>
+            <HeroSection />
         </main>
     );
 };
